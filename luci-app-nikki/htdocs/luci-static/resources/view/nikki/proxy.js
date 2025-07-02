2e4c292f7a17db907b3c51f663f17861484b4c8c
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-07-02 19:13:09 +0800

    Update proxy.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/proxy.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/proxy.js
@@ -26,7 +26,7 @@ return view.extend({
 
         m = new form.Map('nikki');
 
-        s = m.section(form.NamedSection, 'proxy', 'proxy', _('Proxy Config'));
+        s = m.section(form.NamedSection, 'proxy', 'proxy', _('** Proxy Config **'));
 
         s.tab('proxy', _('Proxy Config'));
 
