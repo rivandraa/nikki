78b08254f8a9de05c19fccc7e51992334ff98623
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-07-02 19:10:48 +0800

    Update profile.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/profile.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/profile.js
@@ -15,7 +15,7 @@ return view.extend({
 
         m = new form.Map('nikki');
 
-        s = m.section(form.NamedSection, 'config', 'config', _('Profile'));
+        s = m.section(form.NamedSection, 'config', 'config', _('XXX Profile XXX'));
 
         o = s.option(form.FileUpload, '_upload_profile', _('Upload Profile'));
         o.browser = true;
